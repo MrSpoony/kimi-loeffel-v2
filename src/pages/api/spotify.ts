9995a290655157b735544d739c986fcbbafb5518
MrSpoony <kimi.loeffel@hispeed.ch>
2022-11-03 22:03:18 +0100

    refactor spotify method

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -8,21 +8,28 @@ const spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.SPOTIFY_SECRET,
 });
 
-export default async function spotify(_: NextApiRequest, res: NextApiResponse) {
+async function refreshToken() {
   const x = await spotifyApi.refreshAccessToken();
   spotifyApi.setAccessToken(x.body.access_token);
   spotifyApi.setRefreshToken(x.body.refresh_token || process.env.SPOTIFY_REFRESH_TOKEN || "");
-  spotifyApi.getMyCurrentPlayingTrack().then(data => {
-    const is_playing = data.body.is_playing;
-    if (!is_playing) return res.status(200).json({ is_playing });
-    const song = data.body.item?.name;
-    const album = (data.body.item as SpotifyApi.TrackObjectFull).album?.name;
-    const artists = (data.body.item as SpotifyApi.TrackObjectFull).album.artists.map(a => a?.name);
-    const image = (data.body.item as SpotifyApi.TrackObjectFull).album.images?.[0]?.url;
-    const info = { is_playing, song, album, artists, image };
-    return res.status(200).json(info)
-  }, err => {
-    console.error(err)
+}
+
+export default async function spotify(_: NextApiRequest, res: NextApiResponse) {
+  await refreshToken();
+
+  let data;
+  try {
+    data = await spotifyApi.getMyCurrentPlayingTrack()
+  } catch (e) {
+    console.error(e);
     return res.status(500).json({ message: "Could not load the song" })
-  })
+  }
+  const is_playing = data.body.is_playing;
+  if (!is_playing) return res.status(200).json({ is_playing: false });
+  const song = data.body.item?.name;
+  const album = (data.body.item as SpotifyApi.TrackObjectFull).album?.name;
+  const artists = (data.body.item as SpotifyApi.TrackObjectFull).album.artists.map(a => a?.name);
+  const image = (data.body.item as SpotifyApi.TrackObjectFull).album.images?.[0]?.url;
+  const info = { is_playing, song, album, artists, image };
+  return res.status(200).json(info)
 }
